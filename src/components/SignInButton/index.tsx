98c7791e7d875cd0ec14d4fe50e880ecceb32f35
Jean Martins <jeanmrtns4@gmail.com>
2021-03-27 11:49:44 -0300

    first commit

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,31 @@
+import styles from './styles.module.scss'
+import { FaGithub } from 'react-icons/fa'
+import { useState } from 'react'
+import { MdClose } from 'react-icons/md'
+
+export default function SignInButton() {
+  const [isLogged, setIsLogged] = useState(true)
+
+  function handleLogClick() {
+    setIsLogged(!isLogged)
+  }
+
+  return (
+    <>
+      {!isLogged ? (
+        <button
+          className={`${styles.signInButton} ${styles.buttonContainer}`}
+          onClick={handleLogClick}>
+          <FaGithub />
+          <span>Sign In with GitHub</span>
+        </button>
+      ) : (
+        <button className={`${styles.signOutButton} ${styles.buttonContainer}`}>
+          <FaGithub />
+          <span>jeanmrtns</span>
+          <MdClose className={styles.close} onClick={handleLogClick} />
+        </button>
+      )}
+    </>
+  )
+}
