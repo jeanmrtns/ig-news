b15d475c891702cdbbb1c9ea4909b9087f65e6ff
Jean Martins <jeanmrtns4@gmail.com>
2021-03-30 19:34:45 -0300

    Stripe configurate

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,14 +4,12 @@ import { query as q } from 'faunadb'
 import { fauna } from '../../../services/faunadb'
 
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
       scope: 'read:user',
     }),
-    // ...add more providers here
   ],
   callbacks: {
     async signIn(user, account, profile) {
